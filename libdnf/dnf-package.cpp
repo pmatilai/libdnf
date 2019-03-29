daf93419987d06983578a5a1a99dde886ddbf3da
Aleš Matěj <amatej@redhat.com>
2019-03-29 15:08:37 +0100

    Add support for xml:base attribute from primary.xml (RhBug:1691315)
    Closes: #708
    Approved by: <try>

--- libdnf/dnf-package.cpp
+++ libdnf/dnf-package.cpp
@@ -113,9 +113,14 @@ dnf_package_get_filename(DnfPackage *pkg)
     /* default cache filename location */
     if (priv->filename == NULL && priv->repo != NULL) {
         if (dnf_repo_is_local(priv->repo)) {
-            priv->filename = g_build_filename(dnf_repo_get_location(priv->repo),
+            gchar *url_location = g_strdup(dnf_package_get_baseurl(pkg));
+            if (!url_location){
+                url_location = g_strdup(dnf_repo_get_location(priv->repo));
+            }
+            priv->filename = g_build_filename(url_location,
                                               dnf_package_get_location(pkg),
                                               NULL);
+            g_free(url_location);
         } else {
             /* set the filename to cachedir for non-local repos */
             g_autofree gchar *basename = NULL;
